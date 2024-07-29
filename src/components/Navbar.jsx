bacb31639cca286b377cfc706266c055f30501fc
Joel Bassil <joel.ba247@gmail.com>
2024-07-29 17:10:54 +0100

    Final adjustments to routing and navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
@@ -9,7 +9,6 @@ const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   const [scrolled, setScrolled] = useState(false);
-  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -26,10 +25,6 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const resetGame = () => {
-    localStorage.removeItem('montyHallCompleted');
-  };
-
   return (
     <nav
       className={`${
@@ -40,12 +35,11 @@ const Navbar = () => {
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
-          to='/'
+          to='/Portfolio'
           className='flex items-center gap-2'
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
-            resetGame();
           }}
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
@@ -63,21 +57,9 @@ const Navbar = () => {
               } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(nav.title)}
             >
-              {location.pathname === '/portfolio' ? (
-                <a href={`#${nav.id}`}>{nav.title}</a>
-              ) : (
-                <Link to="/portfolio">{nav.title}</Link>
-              )}
+              <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
-          {location.pathname === '/portfolio' && (
-            <li
-              className="text-secondary hover:text-white text-[18px] font-medium cursor-pointer"
-              onClick={resetGame}
-            >
-              <Link to="/">Play Game</Link>
-            </li>
-          )}
         </ul>
 
         <div className='sm:hidden flex flex-1 justify-end items-center'>
@@ -105,24 +87,9 @@ const Navbar = () => {
                     setActive(nav.title);
                   }}
                 >
-                  {location.pathname === '/portfolio' ? (
-                    <a href={`#${nav.id}`}>{nav.title}</a>
-                  ) : (
-                    <Link to="/portfolio">{nav.title}</Link>
-                  )}
+                  <a href={`#${nav.id}`}>{nav.title}</a>
                 </li>
               ))}
-              {location.pathname === '/portfolio' && (
-                <li
-                  className="font-poppins font-medium cursor-pointer text-[16px] text-secondary"
-                  onClick={() => {
-                    setToggle(!toggle);
-                    resetGame();
-                  }}
-                >
-                  <Link to="/">Play Game</Link>
-                </li>
-              )}
             </ul>
           </div>
         </div>
