a1eb8d59d635d0e83cbac873f25c22ca03f5d36f
Joel Bassil <joel.ba247@gmail.com>
2024-07-29 17:52:01 +0100

    Fix routing for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-404',
+      writeBundle() {
+        fs.copyFileSync(
+          path.resolve(__dirname, 'public/404.html'),
+          path.resolve(__dirname, 'dist/404.html')
+        )
+      }
+    }
+  ],
   base: '/Portfolio/',
 })
