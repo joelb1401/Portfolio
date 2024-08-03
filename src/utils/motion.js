9ef9bbe3036e7314e21b3afcbdb2a96b1540dec0
Joel Bassil <joel.ba247@gmail.com>
2024-08-03 18:05:52 +0100

    fix

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -21,7 +21,7 @@ export const fadeIn = (direction, type, delay, duration) => {
     hidden: {
       x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
       y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
-      opacity: 0,
+      opacity: 0.5,
     },
     show: {
       x: 0,
